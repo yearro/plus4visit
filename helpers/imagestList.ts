5b0b106702efe50015614f6e5ee7ee71d2b3eaff
Yeri Armenta <yeri.armenta@gmail.com>
2025-11-26 12:26:22 -0600

    imageList helper added

---
+++ helpers/imagestList.ts
@@ -0,0 +1,28 @@
+export const satisfactionLevel = [
+    {
+      id: 1,
+      title: 'Excellent',
+      src: require('@/assets/images/5nivel.png'),
+      borderColor: '#5f9869'
+    }, {
+      id: 2,
+      title: 'Good',
+      src: require('@/assets/images/4nivel.png'),
+      borderColor: '#77c17a'
+    }, {
+      id: 3,
+      title: 'Acceptable',
+      src: require('@/assets/images/3nivel.png'),
+      borderColor: '#ebaf36'
+    }, {
+      id: 4,
+      title: 'Fair',
+      src: require('@/assets/images/2nivel.png'),
+      borderColor: '#e3701d'
+    }, {
+      id: 5,
+      title: 'Poor',
+      src: require('@/assets/images/1nivel.png'),
+      borderColor: '#de493d'
+    }
+  ]

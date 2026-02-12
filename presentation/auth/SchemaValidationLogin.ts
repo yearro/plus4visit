3d830ea7db09b60ae58471c99f33e93faaf807e9
Yeri Armenta <yeri.armenta@gmail.com>
2026-02-12 15:43:14 -0600

    Paswword validation schema updated

--- presentation/auth/SchemaValidationLogin.ts
+++ presentation/auth/SchemaValidationLogin.ts
@@ -6,8 +6,12 @@ export const baseLoginSchema = Yup.object().shape({
     .required('Email is required')
     .matches(EMAIL_RE, "Invalid email address"),
   pass: Yup.string()
-    .min(6, 'Password must be at least 6 characters')
-    .required('Password is required'),
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    .matches(/[^\w]/, 'Password requires a symbol'),
 });
 
 export const additionalNewUserSchema = Yup.object().shape({
